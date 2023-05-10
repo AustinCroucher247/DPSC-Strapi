466a9e6a463db799b0ef95f3fb68df71e87ccf96
Austin Croucher <austin.croucher@gmail.com>
2023-05-10 10:22:32 -0400

    changed to pg

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,25 @@
-// path: ./config/env/production/database.js
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-    return {
-        connection: {
-            client: 'sqlite',
-            connection: {
-                host,
-                port,
-                database,
-                user,
-                password,
-                ssl: { rejectUnauthorized: false },
+module.exports = ({ env }) => ({
+    defaultConnection: "default",
+    connections: {
+        default: {
+            connector: "bookshelf",
+            settings: {
+                client: "postgres",
+                host: config.host,
+                port: config.port,
+                database: config.database,
+                username: config.user,
+                password: config.password,
+                ssl: {
+                    rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+                },
             },
-            debug: false,
+            options: {
+                ssl: env.bool("DATABASE_SSL", false),
+            },
         },
-    }
-};
+    },
+});
