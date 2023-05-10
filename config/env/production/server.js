8ba97834d8ea730cc6dd40c90f7909fb82893bcd
Austin Croucher <austin.croucher@gmail.com>
2023-05-10 17:00:42 -0400

    changed server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,17 +1,16 @@
-// Path: ./config/env/production/server.js
-// starting from Strapi v 4.6.1 server.js has to be the following
-
 module.exports = ({ env }) => ({
-    proxy: true,
-    host: "0.0.0.0",
-    port: process.env.PORT,
-    url: env('MY_HEROKU_URL'),
-    app: {
-        keys: env.array('APP_KEYS')
-    },
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL', ''),
     admin: {
         auth: {
-            secret: env('ADMIN_JWT_SECRET'),
+            secret: env('ADMIN_JWT_SECRET', ''),
         },
     },
-})
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
+    webhooks: {
+        populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    },
+});
