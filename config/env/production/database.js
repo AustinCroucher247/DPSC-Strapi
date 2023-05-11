5b8b6a900a406816b01fb8a2a6ed794acdb5912a
Austin Croucher <austin.croucher@gmail.com>
2023-05-11 10:16:22 -0400

    chaning port

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
             settings: {
                 client: "postgres",
                 host: config.host,
-                port: 1337,
+                port: env.int('PORT', 1337),
                 database: config.database,
                 username: config.user,
                 password: config.password,
